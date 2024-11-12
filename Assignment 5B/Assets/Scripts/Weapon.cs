1a8d16b2e029bb2c4221dc5de3b83ac20b19639f
Treasurek04 <treasurekeys2004@gmail.com>
2024-11-12 03:23:14 -0600

    assignment 6

---
+++ Assignment 5B/Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public int damageBonus; 
+
+    public void Recharge()
+    {
+        Debug.Log("Recharging Weapon!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
